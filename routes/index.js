e7d1be58e4516e469b9db1e2425e3e08edcfefba
ericmkchan <113152408+ericmkchan@users.noreply.github.com>
2022-09-28 22:16:47 -0400

    0.4

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', name: 'Eric Chan' });
 });
 
 router.get('/aboutme', function(req, res, next) {
-  res.render('aboutme', { title: 'About Me'});
+  res.render('aboutme', { title: 'About Me', name: 'Eric Chan'});
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects'});
+  res.render('projects', { title: 'Projects', name: 'Eric Chan'});
 });
 
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services'});
+  res.render('services', { title: 'Services', name: 'Eric Chan'});
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact'});
+  res.render('contact', { title: 'Contact', name: 'Eric Chan'});
 });
 
 module.exports = router;
